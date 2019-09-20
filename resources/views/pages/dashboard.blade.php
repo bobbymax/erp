a09caf5f053607111efcbf696313c36050c526cf
Bobby Ekaro <iambobbymax@gmail.com>
2019-09-20 10:38:25 +0100

    portal updates

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -3,50 +3,25 @@
 <!-- sales report area start -->
     <div class="sales-report-area mt-5 mb-5">
         <div class="row">
-            <div class="col-md-4">
-                <div class="single-report mb-xs-30">
-                    <div class="s-report-inner pr--20 pt--30 mb-3">
-                        <div class="icon"><i class="fa fa-users"></i></div>
-                        <div class="s-report-title d-flex justify-content-between">
-                            <h4 class="header-title mb-0">Users</h4>
-                            <p>online</p>
-                        </div>
-                        <div class="d-flex justify-content-between pb-2">
-                            <h2>$ 4567809,987</h2>
-                            <span>- 45.87</span>
-                        </div>
+            @if (!is_object(auth()->user()->profile))
+                <div class="col-md-12 mb-5">
+                    <div class="alert alert-danger" role="alert">
+                        <h4 class="alert-heading">Update your User Profile!!</h4>
+                        <p>Please update your profile details in order to enjoy the features of this application.</p>
+                        <br>
+                        <a href="#" class="btn btn-danger btn-xs">Go to Profile</a>
                     </div>
                 </div>
+            @endif
+
+            <div class="col-md-4">
+                <a href="{{ route('tickets.create') }}" class="btn btn-block btn-info">Create Support Ticket</a>
             </div>
             <div class="col-md-4">
-                <div class="single-report mb-xs-30">
-                    <div class="s-report-inner pr--20 pt--30 mb-3">
-                        <div class="icon"><i class="fa fa-hourglass-start"></i></div>
-                        <div class="s-report-title d-flex justify-content-between">
-                            <h4 class="header-title mb-0">Tasks</h4>
-                            <p>unattended</p>
-                        </div>
-                        <div class="d-flex justify-content-between pb-2">
-                            <h2>$ 4567809,987</h2>
-                            <span>- 45.87</span>
-                        </div>
-                    </div>
-                </div>
+                <a href="{{ route('trainings.create') }}" class="btn btn-block btn-primary">Add Training</a>
             </div>
             <div class="col-md-4">
-                <div class="single-report">
-                    <div class="s-report-inner pr--20 pt--30 mb-3">
-                        <div class="icon"><i class="fa fa-ticket"></i></div>
-                        <div class="s-report-title d-flex justify-content-between">
-                            <h4 class="header-title mb-0">Tickets</h4>
-                            <p>unassigned</p>
-                        </div>
-                        <div class="d-flex justify-content-between pb-2">
-                            <h2>$ 4567809,987</h2>
-                            <span>- 45.87</span>
-                        </div>
-                    </div>
-                </div>
+                <a href="{{ route('create.proposed') }}" class="btn btn-block btn-info">Propose Training</a>
             </div>
         </div>
     </div>
